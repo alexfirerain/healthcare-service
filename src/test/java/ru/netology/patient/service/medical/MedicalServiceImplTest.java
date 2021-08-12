14f27707be7f8fdd9ea4585c6a83303c50135286
Alexander Lukyanov <lux-nova@mail.ru>
2021-08-12 13:16:57 +0300

    test issues fixed!

--- src/test/java/ru/netology/patient/service/medical/MedicalServiceImplTest.java
+++ src/test/java/ru/netology/patient/service/medical/MedicalServiceImplTest.java
@@ -1,12 +1,10 @@
 package ru.netology.patient.service.medical;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
-import ru.netology.patient.entity.BloodPressure;
-import ru.netology.patient.entity.HealthInfo;
-import ru.netology.patient.entity.PatientInfo;
+import ru.netology.patient.entity.*;
 import ru.netology.patient.repository.PatientInfoRepository;
 import ru.netology.patient.service.alert.SendAlertService;
 
@@ -26,34 +24,30 @@ class MedicalServiceImplTest {
     static final PatientInfo PATIENT_INFO = new PatientInfo(PATIENT_ID,
             "Имя", "Фамилия", LocalDate.of(2000, 1, 1),
             new HealthInfo(new BigDecimal("36.6"), new BloodPressure(110, 70)));
+    static ArgumentCaptor<String> msgCaptor;
 
-    @BeforeAll
-    static void setUp() {
+    // проблема решена, все тесты проходят!
+    // оказалось, что инициализацию моков просто следует делать перед каждым
+    @BeforeEach
+    void setMock() {
         repoMock = Mockito.mock(PatientInfoRepository.class);
         alertMock = Mockito.mock(SendAlertService.class);
         medicalService = new MedicalServiceImpl(repoMock, alertMock);
         Mockito.when(repoMock.getById(PATIENT_ID)).thenReturn(PATIENT_INFO);
+        msgCaptor = ArgumentCaptor.forClass(String.class);
     }
 
     @Test
     void warns_if_pressure_issue() {
-        ArgumentCaptor<String> msgCaptor = ArgumentCaptor.forClass(String.class);
-
         medicalService.checkBloodPressure(PATIENT_ID, new BloodPressure(120, 80));
-
         Mockito.verify(alertMock, Mockito.only()).send(msgCaptor.capture());
-
         assertEquals(ALARM_MSG, msgCaptor.getValue());
     }
 
     @Test
     void warns_if_temperature_issue() {
-        ArgumentCaptor<String> msgCaptor = ArgumentCaptor.forClass(String.class);
-
         medicalService.checkTemperature(PATIENT_ID, new BigDecimal("35"));
-
         Mockito.verify(alertMock, Mockito.only()).send(msgCaptor.capture());
-
         assertEquals(ALARM_MSG, msgCaptor.getValue());
     }
 
